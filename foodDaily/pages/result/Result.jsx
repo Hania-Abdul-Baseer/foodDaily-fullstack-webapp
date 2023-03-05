8b76556b21abb6a0ba15156951f14b3fbcaa616b
ArpanMukhopadhyay <amukhopadhyay02@gmail.com>
2023-03-05 14:10:48 +1030

    pl

--- foodDaily/pages/result/Result.jsx
+++ foodDaily/pages/result/Result.jsx
@@ -7,15 +7,14 @@ import breakfast from './assets/breakfast.svg'
 import lunch from './assets/lunch.svg'
 import dinner from './assets/dinner.svg'
 
-console.log(breakfast,lunch,dinner)
-function Result() {
+function Result({output}) {
   return (
     <>
     <Title></Title>
     <Types diet="Vegan" age="21" goal="Lose Weight" physical="Active" cuisine="Chinese"></Types>
-    <Feed title= "Breakfast" link='/_next/static/media/breakfast.2b93a104.svg'></Feed>
-    <Feed title= "Lunch" link='/_next/static/media/lunch.176b852f.svg'></Feed>
-    <Feed title= "Dinner" link= '/_next/static/media/dinner.5b8bff3b.svg'></Feed>
+    <Feed title= "Breakfast" link='/_next/static/media/breakfast.2b93a104.svg' plan={output}></Feed>
+    <Feed title= "Lunch" link='/_next/static/media/lunch.176b852f.svg' plan={output} ></Feed>
+    <Feed title= "Dinner" link= '/_next/static/media/dinner.5b8bff3b.svg' plan={output} ></Feed>
     </>
   );
 }
